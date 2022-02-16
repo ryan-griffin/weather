21048c6c8a8d0125a54e36df8149f2d466bca79d
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-02-16 09:54:59 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ export async function getServerSideProps(): Promise<{
     props: { data: string };
 }> {
     const key: string = "7bfe2a9d6e00e1f0d6974f869e519a07";
-    const lon: string = "42.2959";
-    const lat: string = "71.7128";
+    const lat: string = "71.5523";
+    const lon: string = "42.3459";
 
     const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?&units=imperial&lat=${lat}&lon=${lon}&appid=${key}`
+        `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${lat}&lon=${lon}&appid=${key}`
     );
     const data = await res.json();
     return {
@@ -20,13 +20,13 @@ export async function getServerSideProps(): Promise<{
 
 const Home: NextPage<Props> = ({ data }) => (
     <main>
-        <h1>{data.main.temp}°F</h1>
+        <h1>{data.current.temp}°F</h1>
     </main>
 );
 
 interface Props {
     data: {
-        main: {
+        current: {
             temp: string;
         };
     };
