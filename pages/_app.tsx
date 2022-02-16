711ff45a408be52150dc32254e0f0f650e36f631
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-02-15 21:54:19 -0500

    api works now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import "./index.scss";
 
 function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import { NextPage } from "next";
+
+export async function getServerSideProps() {
+    const key: string = "7bfe2a9d6e00e1f0d6974f869e519a07";
+    const lon: string = "42.2959";
+    const lat: string = "71.7128";
+
+    const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?&units=imperial&lat=${lat}&lon=${lon}&appid=${key}`
+    );
+    const data = await res.json();
+    return {
+        props: {
+            data,
+        },
+    };
+}
+
+const Home: NextPage<Props> = ({ data }) => (
+    <main>
+        <h1>{data.main.temp}°F</h1>
+    </main>
+);
+
+interface Props {
+    data: {
+        main: {
+            temp: string;
+        };
+    };
+}
+
+export default Home;
