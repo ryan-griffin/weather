ee1e4641f159cdcc0e2f9cbc5333aee3bf71fd12
ryan-griffin <ryan-griffin@users.noreply.github.com>
2022-02-21 23:47:35 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,40 +27,57 @@ export async function getServerSideProps(): Promise<{
     };
 }
 
-const Home: NextPage<Props> = ({ data }) => (
-    <main className="flex flex-col justify-center items-center w-screen h-screen bg-gradient-to-br from-blue-400 to-blue-500">
-        <h1 className="text-4xl font-semibold">{data.location[0].name}</h1>
-        <div className="flex items-center">
-            <Image
-                src={`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`}
-                height={128}
-                width={128}
-                alt=""
-            />
-            <h1 className="text-8xl font-semibold">{data.current.temp}&deg;</h1>
-        </div>
-        <h1 className="text-4xl font-semibold mb-6">
-            {data.current.weather[0].description}
-        </h1>
-        <div className="flex gap-10">
-            <h1 className="text-4xl font-semibold">
-                L:{data.daily[0].temp.min}&deg;
+const Home: NextPage<Props> = ({ data }) => {
+    const date = new Date();
+    const hour = date.getHours();
+    let gradient: string | undefined;
+    if (hour >= 20 || hour <= 4) {
+        gradient = "bg-gradient-to-br from-gray-800 to-gray-900";
+    } else if (hour >= 5 && hour <= 13) {
+        gradient = "bg-gradient-to-br from-sky-400 to-sky-600";
+    } else if (hour >= 14 && hour <= 19) {
+        gradient = "bg-gradient-to-br from-sky-700 to-sky-900";
+    }
+
+    return (
+        <main
+            className={`flex flex-col justify-center items-center w-screen h-screen ${gradient}`}
+        >
+            <h1 className="text-4xl font-semibold">{data.location[0].name}</h1>
+            <div className="flex items-center">
+                <Image
+                    src={`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`}
+                    height={128}
+                    width={128}
+                    alt=""
+                />
+                <h1 className="text-8xl font-semibold">
+                    {data.current.temp}&deg;
+                </h1>
+            </div>
+            <h1 className="text-4xl font-semibold mb-6">
+                {data.current.weather[0].description}
             </h1>
-            <h1 className="text-4xl font-semibold">
-                H:{data.daily[0].temp.max}&deg;
-            </h1>
-        </div>
-    </main>
-);
+            <div className="flex gap-10">
+                <h1 className="text-4xl font-semibold">
+                    L:{data.daily[0].temp.min}&deg;
+                </h1>
+                <h1 className="text-4xl font-semibold">
+                    H:{data.daily[0].temp.max}&deg;
+                </h1>
+            </div>
+        </main>
+    );
+};
 
 interface Props {
     data: {
-        location: Array<any>;
+        location: any[];
         current: {
             temp: string;
-            weather: Array<any>;
+            weather: any[];
         };
-        daily: Array<any>;
+        daily: any[];
     };
 }
 
